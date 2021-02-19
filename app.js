a95d0e1e1d185bb2111d8f1767010802e56960fe
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-19 07:41:23 +0100

    added express-messages middleware

--- app.js
+++ app.js
@@ -73,6 +73,13 @@ app.use(expressValidator({
     }
 }));
 
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
 app.get('/', function(req, res){
     res.render('index');
 })
